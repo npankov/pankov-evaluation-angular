db311b5755ebd362c68352c8e4a91bfc42a03563
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-25 11:18:21 +0100

    Exercice 6 : Liste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pankov-evaluation-angular';
-  firstStatistique: Statistique = new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb46', 'Démographie en France', '60M');
-  secondStatistique: Statistique = new Statistique('e3b-11eb-91ec-7', 'Démographie en Allemagne', '80M');
+  listStatistiques: Statistique[] = [
+    new Statistique('fa1f5f40-be3b-11eb-91ec-7f5875ecfb46', 'Démographie en France', '60M'),
+    new Statistique('e3b-11eb-91ec-7', 'Démographie en Allemagne', '80M')
+  ];
 
   constructor() {
 
